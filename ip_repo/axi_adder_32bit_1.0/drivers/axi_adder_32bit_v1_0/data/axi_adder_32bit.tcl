363b723ed623d4a72633f4f4e1f142a6a089cd6f
Josh Hollis <joshuahollis608@gmail.com>
2024-03-07 11:59:20 -0600

    reset

--- ip_repo/axi_adder_32bit_1.0/drivers/axi_adder_32bit_v1_0/data/axi_adder_32bit.tcl
+++
@@ -1,5 +0,0 @@
-
-
-proc generate {drv_handle} {
-	xdefine_include_file $drv_handle "xparameters.h" "axi_adder_32bit" "NUM_INSTANCES" "DEVICE_ID"  "C_S00_AXI_BASEADDR" "C_S00_AXI_HIGHADDR"
-}
--- picorv32_project1/helloworld/read_mem.tcl
+++
@@ -1,5 +0,0 @@
-reset_hw_axi [get_hw_axis hw_axi_1]
-set address 0x00100000
-set rt axi_bram_rt
-create_hw_axi_txn $rt [get_hw_axis hw_axi_1] -type read -address $address -len 4 -force
-run_hw_axi [get_hw_axi_txns $rt]
